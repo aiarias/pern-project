c3d576800c4128fb34959513b432554a8b70780a
aiarias <aiarias@uc.cl>
2024-07-30 15:21:29 -0400

    routes added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import taskRoutes from "./routes/tasks.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+
+// Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //TODO: se envia folmulario simple
 
+// Routes
 app.get("/", (req, res) => res.json({ message: "Welcome to my API" }));
+app.use('/api', taskRoutes);
+app.use('/api', authRoutes);
 
+// Error handler
 app.use((err, req, res, next) => {
   res.status(500).json({
     status: "error",
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+import Router from "express";
+
+const router = Router();
+
+router.post("/signin", (req, res) => res.send("Ingresando"));
+
+router.post("/signup", (req, res) => res.send("Registrando"));
+
+router.post("/signout", (req, res) => res.send("Cerrando sesion"));
+
+router.get("/profile", (req, res) => res.send("Perfil"));
+
+
+
+export default router;
