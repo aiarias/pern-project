5d4930a0e52f2d1a93bf53d8b22ae98165b08497
aiarias <aiarias@uc.cl>
2024-08-03 20:33:17 -0400

    Register Page ok - NotFound page ok LoginPage in process

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,48 @@
+import { Card, Input, Button, Label } from "../components/ui";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 function LoginPage() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  })
+  return (
+    <div className="h-[calc(100vh-64px)] flex justify-center items-center">
+      <Card>
+        <h1 className="text-4xl font-bold my-2 text-center">Sign in</h1>
+
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            placeholder="Email"
+            {...register("email", {
+              required: true,
+            })}
+          />
+
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: true,
+            })}
+          />
+
+          <Button>Sign in</Button>
+
+          <div className="flex justify-between my-4">
+            <p>Dont have an account?</p>
+            <Link to="/register" className="font-bold text-sky-600">
+              Register
+            </Link>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default LoginPage;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,70 @@
+import { Input, Card, Button, Label } from "../components/ui";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Link } from "react-router-dom";
 function RegisterPage() {
-    return (
-        <div>
-            <h1>Register Page</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = handleSubmit(async (data) => {
+
+    const res = await axios.post("http://localhost:3000/api/signup", data, {
+      withCredentials: true,
+    
+    }); // Aqui se hace la peticion hacia el backend
+    console.log(res);
+  });
+
+  return (
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h3 className="text-2xl font-bold text-center">Register</h3>
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            placeholder="enter your fullname"
+            {...register("name", {
+              required: true,
+            })}
+          />
+          {errors.name && <p className="text-red-500">Name is required</p>}
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            placeholder="enter your email"
+            {...register("email", {
+              required: true,
+            })}
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            placeholder="enter your password"
+            {...register("password", {
+              required: true,
+            })}
+          />
+
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+
+          <Button>Register</Button>
+
+          <div className="flex justify-between my-4"> 
+            <p>Do have an account</p>
+            <Link to="/login" className="font-bold text-sky-600">
+              Sign in
+            </Link>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default RegisterPage;
